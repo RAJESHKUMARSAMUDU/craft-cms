04abf78d020e3c8a9c8a618af8d096342013b7af
RAJESHKUMARSAMUDU <69385264+RAJESHKUMARSAMUDU@users.noreply.github.com>
2021-03-30 13:34:54 +0530

    Update db.php

--- db.php
+++ db.php
@@ -9,19 +9,14 @@
 
 return array(
 
-        // The database server name or IP address. Usually this is 'localhost' or '127.0.0.1'.
-        'server' => .$_ENV["CRAFT_DATABASE_HOST"],
+	'server' => getenv('CRAFT_DATABASE_HOST'),
 
-        // The name of the database to select.
-        'database' => .$_ENV["CRAFT_DATABASE_NAME"],
+	'database' => getenv('CRAFT_DATABASE_NAME'),
 
-        // The database username to connect with.
-        'user' => .$_ENV["CRAFT_DATABASE_USER"],
+	'user' => getenv('CRAFT_DATABASE_USER'),
 
-        // The database password to connect with.
-        'password' => .$_ENV["CRAFT_DATABASE_PASSWORD"],
+	'password' => getenv('CRAFT_DATABASE_PASSWORD'),
 
-        // The prefix to use when naming tables. This can be no more than 5 characters.
-        'tablePrefix' => 'craft',
+	'tablePrefix' => 'craft',
 
 );
