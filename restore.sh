e8a73e3a84b1d0503dadb4bc4cc300bf7ffed611
RAJESHKUMARSAMUDU <69385264+RAJESHKUMARSAMUDU@users.noreply.github.com>
2022-02-25 16:06:53 +0530

    Create restore.sh

---
+++ restore.sh
@@ -0,0 +1,20 @@
+aws rds restore-db-cluster-from-snapshot --db-cluster-identifier restore-plsql --snapshot-identifier rajesh-pgsql-manual-2022-02-24-13-46 --engine aurora-postgresql  --vpc-security-group-ids sg-0e853a914be96db97 --db-subnet-group-name rds-private-subnet-group
+
+aws rds create-db-instance --db-instance-identifier restore-plsql-instance-1 --db-instance-class db.t3.medium --engine aurora-postgresql --db-subnet-group-name rds-private-subnet-group --db-cluster-identifier restore-plsql
+
+aws rds describe-db-instances --db-instance-identifier restore-plsql-instance-1 --query 'DBInstances[*].[DBInstanceStatus]'
+
+
+while state=$(aws rds describe-db-instances --db-instance-identifier restore-plsql-instance-1 --query 'DBInstances[*].[DBInstanceStatus]' --output=text); do
+  if [ "$state" = "available" ]; then
+    echo "available"
+    PGPASSWORD=rajesh233 pg_dump -h ${MYSQL_HOST} -U ${MYSQL_USER} ${DATABASE_NAME} >  dump.sql
+    echo "dump_done"
+    PGPASSWORD=rajesh233  psql -h ${MYSQL_HOST_1} -U ${MYSQL_USER} -d ${DATABASE_NAME} <  dump.sql
+    echo "restore_done"
+    break
+  else
+    echo "unavailable"
+  fi
+  sleep 30;
+done
