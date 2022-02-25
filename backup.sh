3f368e7ee902d95a16575055b5d8daff575e1d95
RAJESHKUMARSAMUDU <69385264+RAJESHKUMARSAMUDU@users.noreply.github.com>
2022-02-25 18:22:40 +0530

    Update backup.sh

--- backup.sh
+++ backup.sh
@@ -2,15 +2,19 @@
 
 NOW_DATE=$(date '+%Y-%m-%d-%H-%M')
 
-
-
 RESTORE_FROM_INSTANCE_ID='rajesh-pgsql'
 
 echo "Creating manual snapshot of ${RESTORE_FROM_INSTANCE_ID}"
-SNAPSHOT_ID=$(aws rds create-db-cluster-snapshot --db-cluster-snapshot-identifier ${RESTORE_FROM_INSTANCE_ID}-manual-${NOW_DATE} --db-cluster-identifier ${RESTORE_FROM_INSTANCE_ID})
+
+SNAPSHOT_ID=$(aws rds create-db-cluster-snapshot \
+        --db-cluster-snapshot-identifier ${RESTORE_FROM_INSTANCE_ID}-manual-${NOW_DATE} \
+        --db-cluster-identifier ${RESTORE_FROM_INSTANCE_ID})
 
 echo $SNAPSHOT_ID
-while state=$(aws rds describe-db-cluster-snapshots --db-cluster-snapshot-identifier ${RESTORE_FROM_INSTANCE_ID}-manual-${NOW_DATE} --query 'DBClusterSnapshots[0].[Status]' --output text); do
+
+while state=$(aws rds describe-db-cluster-snapshots \
+        --db-cluster-snapshot-identifier ${RESTORE_FROM_INSTANCE_ID}-manual-${NOW_DATE} \
+        --query 'DBClusterSnapshots[0].[Status]' --output text); do
   if [ "$state" = "available" ]; then
     echo "Finished creating new snapshot ${RESTORE_FROM_INSTANCE_ID}-manual-${NOW_DATE} from ${RESTORE_FROM_INSTANCE_ID}"
     break
